b7164ff0f906dabefe942228e016dd6bd80cff2a
jun.kang <wishful1826@naver.com>
2024-10-21 23:11:10 +0900

    feat(calculator):잘못된 입력에 대한 예외처리 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,20 +6,34 @@ public class Calculator {
             return 0;
         }
 
-        String delimiter = ",|:";
+        String delimiter = "[,:]";
         if (input.startsWith("//")) {
             int delimiterIndex = input.indexOf("\n");
             delimiter = input.substring(2, delimiterIndex);
             input = input.substring(delimiterIndex + 1);
         }
 
-        String[] numbers = input.split(",|:");
+        String[] numbers = input.split(delimiter);
 
         int sum = 0;
         for(String number : numbers) {
-            sum += Integer.parseInt(number);
+            int num = toPositiveNumber(number);
+            sum += num;
         }
 
         return sum;
+    }
+
+    private int toPositiveNumber(String number) {
+        int num;
+        try {
+            num = Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        if (num < 0) {
+            throw new IllegalArgumentException();
+        }
+        return num;
     }
 }
