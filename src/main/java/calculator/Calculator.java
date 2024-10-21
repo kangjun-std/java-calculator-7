8afc0d40d3aaf5781de6eca0a0a44da26ef6e51e
jun.kang <wishful1826@naver.com>
2024-10-21 22:37:29 +0900

    feat(calculator):빈 문자열을 0으로 계산

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,7 +2,9 @@ package calculator;
 
 public class Calculator {
     public int add(String input) {
-
+        if(input.isEmpty()){
+            return 0;
+        }
         int sum = 0;
 
         return sum;
