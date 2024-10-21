7db0979c5e95d6b3d0854b3b598f53d18ed7bb75
jun.kang <wishful1826@naver.com>
2024-10-21 22:59:22 +0900

    feat(calculator):커스텀 구분자 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,9 +5,20 @@ public class Calculator {
         if(input.isEmpty()){
             return 0;
         }
+
+        String delimiter = ",|:";
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\n");
+            delimiter = input.substring(2, delimiterIndex);
+            input = input.substring(delimiterIndex + 1);
+        }
+
         String[] numbers = input.split(",|:");
 
         int sum = 0;
+        for(String number : numbers) {
+            sum += Integer.parseInt(number);
+        }
 
         return sum;
     }
