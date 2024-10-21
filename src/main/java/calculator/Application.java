6a57cea6a0495db8e49033a0a230660929bc8388
jun.kang <wishful1826@naver.com>
2024-10-21 22:33:37 +0900

    feat(calculator):덧셈 결과를 출력할 계산기 구조 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,6 +7,9 @@ public class Application {
         System.out.print("덧셈할 문자열을 입력해 주세요: ");
         String input = Console.readLine();
 
-        
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(input);
+        System.out.print("결과: " + result);
     }
 }
---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class Calculator {
+    public int add(String input) {
+
+        int sum = 0;
+
+        return sum;
+    }
+}
